5bb3a180b485da84dfd0276480555cd0e3e0178a
Onegreat <wangreat@yandex.ru>
2022-12-24 18:11:48 +0300

    Создал функицию конструктор

---
+++ script.js
@@ -0,0 +1,9 @@
+'use strict'
+
+const DomElement = function (selector, height, width, bg, fontSize) {
+    this.selector = selector,
+        this.height = height,
+        this.width = width,
+        this.bg = bg,
+        this.fontSize = fontSize
+}

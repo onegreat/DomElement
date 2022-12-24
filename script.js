79dbb8b536b1bd86ac658058b4b5b8b5cd40c65b
Onegreat <wangreat@yandex.ru>
2022-12-24 18:21:28 +0300

    Реализовал условие

--- script.js
+++ script.js
@@ -6,4 +6,24 @@ const DomElement = function (selector, height, width, bg, fontSize) {
         this.width = width,
         this.bg = bg,
         this.fontSize = fontSize
+
+    this.forFunc = function () {
+        let elem;
+
+        if (this.selector[0] === '.') {
+            elem = document.createElement('div')
+            elem.className = this.selector.splice(1)
+        } else if (this.selector[0] === '#') {
+            elem = document.createElement('p');
+            elem.id = this.selector.slice(1);
+        } else {
+            elem.style.cssText = `height: ${this.height}px;
+            width: ${this.width}px;
+            background: ${this.bg};
+            font-size: ${this.fontSize}px;`
+
+            return elem;
+
+        }
+    }
 }
